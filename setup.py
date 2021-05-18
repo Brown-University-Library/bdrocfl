94b66b59abbb60dd641b0a6e9fe4f0d139a9a075
Ben Cail <benjamin_cail@brown.edu>
2021-05-18 16:37:57 -0400

    update version to 0.2.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdrocfl",
-    version='0.1',
+    version='0.2.dev',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[],
