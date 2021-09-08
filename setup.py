8ec02251804267e3a7359fc3f22c37474e73aa6d
Ben Cail <benjamin_cail@brown.edu>
2021-09-08 15:52:18 -0400

    version 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdrocfl",
-    version='0.6.dev',
+    version='0.6',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[],
